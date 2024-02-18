5b1a56f1077f31aeccf3dbcc4bbea4a035479607
Patrick Douglas <pdouglas.s@hotmail.com>
2024-02-18 19:24:29 -0300

    Criando middleware para autenticacao

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import { Router, Request, Response } from 'express'
 import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController';
+import { DetailUserController } from './controllers/user/DetailUserController';
+import { isAuthenticate } from './middlewares/isAuthenticate';
 
 const router = Router();
 
 // Rotas usuários
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
+router.get('/me', isAuthenticate, new DetailUserController().handle)
 
 export { router };

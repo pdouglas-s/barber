523de496d8a261a4c02b250ae5f13cfd4f28c485
Patrick Douglas <pdouglas.s@hotmail.com>
2024-02-18 19:48:45 -0300

    Detalhes do usuario

--- src/controllers/user/DetailUserController.ts
+++ src/controllers/user/DetailUserController.ts
@@ -3,10 +3,11 @@ import { UserDetailService } from "../../services/user/DetailUserService";
 
 class DetailUserController{
   async handle( request: Request, response: Response){
+    const user_id = request.user_id;
 
     const userDetailService = new UserDetailService();
 
-    const detailUser = await userDetailService.execute();
+    const detailUser = await userDetailService.execute(user_id);
 
     return response.json(detailUser)
   }
